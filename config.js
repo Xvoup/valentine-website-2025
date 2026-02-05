9e01da759642c2fe5d343d1281803014a2b809e6
Xvoup <rosabriseno28@gmail.com>
2026-02-04 23:21:09 -0800

    Update config.js

--- config.js
+++ config.js
@@ -42,7 +42,7 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much?",  // Shows when they go past 5000%
+        extreme: "WOOOOW You love me that much? 🥹🤫❤️",  // Shows when they go past 5000%
         high: "yesss! 🤫❤️‍🩹",              // Shows when they go past 1000%
         normal: "yes! ❤️"                           // Shows when they go past 100%
     },
